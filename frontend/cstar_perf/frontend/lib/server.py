2d96524d32fb7bad7892bc75a84da5734c767feb
Russ Hatch <rhatch@datastax.com>
2014-10-01 17:19:12 -0600

    block frontend server startup until notification server is running

--- frontend/cstar_perf/frontend/lib/server.py
+++ frontend/cstar_perf/frontend/lib/server.py
@@ -4,20 +4,29 @@ import sys
 import shlex
 import subprocess
 import argparse
+import logging
 
 from cstar_perf.frontend.server.util import create_app_config
 from cstar_perf.frontend.lib.crypto import generate_server_keys
+from cstar_perf.frontend.server.notifications import console_publish
+
+log = logging.getLogger('cstar_perf.frontend.lib.server')
+
 
 def run_server():
     # Initialize database before gunicorn workers startup:
     from cstar_perf.frontend.server.model import Model
     db = Model()
     del db
-    
+
     app_path = os.path.realpath(os.path.join(os.path.dirname(
         os.path.realpath(__file__)), os.path.pardir, "server"))
     os.chdir(app_path)
-    
+
+    log.info('Waiting for cstar_perf_notifications startup...')
+    # this will block until cstar_perf_notifications is up and running
+    console_publish('dummy_cluster', {'job_id': 'startup_check', 'msg': 'checking for notification server'})
+
     proc = subprocess.Popen(shlex.split("gunicorn -k flask_sockets.worker --log-file=- --workers=10 app:app"))
 
     # Capture SIGTERM events to shutdown child gunicorn processes..
