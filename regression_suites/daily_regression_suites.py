a3a96de3beb680c9cc6acc951191f16aa6d5e65e
Jim Witschey <jim.witschey@gmail.com>
2015-06-10 11:57:19 -0400

    use product in config (+23 squashed commits) Squashed commits: [f408d1e] adds series functionality [1aed906] switch to default blade 11b [8782dc7] new shas for test history [01c3816] specify compaction strategy classpaths [14776c7] whoops [dd9cae5] refactor day delta stuff, fix -schema option [33e2400] wip [ad85fcd] wip [43acb1c] wip [4f3e453] fix sha logic and imports [b64e365] adds list of uuids [462692b] wip [d8189fe] wip [6f67fee] remove invalid git revisions [7a9ba7f] uses 10M rows for daily regression tests [e9a35c6] adds test suite for daily regressions [2a15789] removes 2000-column test that never works [626cedd] adds profiles for different numbers of columns [cc5a60f] changes batch window to match recommended default [fec23c0] fix compaction strategy formatting [624b2d3] properly pass title to baseline configs [43b5967] adds profiles for compaction strategies [9815093] adds more & more customizable profiles

--- regression_suites/daily_regression_suites.py
+++ regression_suites/daily_regression_suites.py
@@ -1,44 +1,56 @@
 import datetime
-from util import get_tagged_releases, get_branches
+from util import get_sha_from_build_days_ago
 from cstar_perf.frontend.client.schedule import Scheduler
+# import requests
 
+REVISION = 'apache/trunk'
 CSTAR_SERVER = "cstar.datastax.com"
+day_deltas = [7, 14]
+OLD_SHAS = dict(zip(day_deltas,
+                    get_sha_from_build_days_ago('http://' + CSTAR_SERVER,
+                                                day_deltas=day_deltas,
+                                                revision=REVISION)))
+DEFAULT_CLUSTER_NAME = 'blade_11_b'
+
 
 def create_baseline_config(title=None):
     """Creates a config for testing the latest dev build(s) against stable and oldstable"""
-    
-    dev_revisions = ['apache/trunk'] + get_branches()[:2]
-    stable = get_tagged_releases('stable')[0]
-    oldstable = get_tagged_releases('oldstable')[0]
+
+    dev_revisions = dict({0: REVISION}, **OLD_SHAS)
 
     config = {}
 
-    config['revisions'] = revisions = []
-    for r in dev_revisions:
-        revisions.append({'revision': r, 'label': r +' (dev)'})
-    revisions.append({'revision': stable, 'label': stable+' (stable)'})
-    revisions.append({'revision': oldstable, 'label': oldstable+' (oldstable)'})
-    for r in revisions:
+    config['revisions'] = []
+    for days_ago, revision in sorted(dev_revisions.items()):
+        label = REVISION if days_ago == 0 else '{REVISION} ~{days_ago} days ago'.format(REVISION=REVISION,
+                                                                                        days_ago=days_ago)
+        config['revisions'].append({'revision': revision, 'label': label})
+    for r in config['revisions']:
         r['options'] = {'use_vnodes': True}
-        r['java_home'] = "~/fab/jvms/jdk1.7.0_71" if 'oldstable' in r['label'] else "~/fab/jvms/jdk1.8.0_45"
+        r['java_home'] = ("~/fab/jvms/jdk1.7.0_71"
+                          if 'oldstable' in r['label']
+                          else "~/fab/jvms/jdk1.8.0_45")
+        r['testseries'] = 'daily_regressions_trunk'
 
-    config['title'] = 'Jenkins C* regression suite - {}'.format(datetime.datetime.now().strftime("%Y-%m-%d"))
+    config['title'] = 'Daily C* regression suite - {}'.format(datetime.datetime.now().strftime("%Y-%m-%d"))
+    config['product'] = 'cassandra'
 
     if title is not None:
         config['title'] += ' - {title}'.format(title=title)
 
     return config
 
-def test_simple_profile(title='Read/Write', cluster='blade_11', load_rows=65000000, read_rows=65000000, threads=300, yaml=None):
+
+def test_simple_profile(title='Read/Write', cluster=DEFAULT_CLUSTER_NAME, load_rows=65000000, read_rows=65000000, threads=300, yaml=None):
     """Test the basic stress profile with default C* settings"""
     config = create_baseline_config(title)
     config['cluster'] = cluster
     config['operations'] = [
-        {'operation':'stress',
+        {'operation': 'stress',
          'command': 'write n={load_rows} -rate threads={threads}'.format(**locals())},
-        {'operation':'stress',
+        {'operation': 'stress',
          'command': 'read n={read_rows} -rate threads={threads}'.format(**locals())},
-        {'operation':'stress',
+        {'operation': 'stress',
          'command': 'read n={read_rows} -rate threads={threads}'.format(**locals())}
     ]
     if yaml:
@@ -47,7 +59,8 @@ def test_simple_profile(title='Read/Write', cluster='blade_11', load_rows=650000
     scheduler = Scheduler(CSTAR_SERVER)
     scheduler.schedule(config)
 
-def compaction_profile(title='Compaction', cluster='blade_11', rows=65000000, threads=300):
+
+def compaction_profile(title='Compaction', cluster=DEFAULT_CLUSTER_NAME, rows=65000000, threads=300):
     config = create_baseline_config(title)
     config['cluster'] = cluster
     config['operations'] = [
@@ -65,10 +78,12 @@ def compaction_profile(title='Compaction', cluster='blade_11', rows=65000000, th
     scheduler = Scheduler(CSTAR_SERVER)
     scheduler.schedule(config)
 
-def test_compaction_profile():
-    compaction_profile()
 
-def repair_profile(title='Repair', cluster='blade_11', rows=65000000, threads=300):
+def test_compaction_profile():
+    compaction_profile(rows='10M')
+
+
+def repair_profile(title='Repair', cluster=DEFAULT_CLUSTER_NAME, rows=65000000, threads=300):
     config = create_baseline_config(title)
     config['cluster'] = cluster
     config['operations'] = [
@@ -86,21 +101,24 @@ def repair_profile(title='Repair', cluster='blade_11', rows=65000000, threads=30
     scheduler = Scheduler(CSTAR_SERVER)
     scheduler.schedule(config)
 
-def test_repair_profile():
-    repair_profile()
 
-def compaction_strategies_profile(title='Compaction Strategy', cluster='blade_11', rows=65000000, threads=300, strategy=None):
+def test_repair_profile():
+    repair_profile(rows='10M')
+
+
+def compaction_strategies_profile(title='Compaction Strategy', cluster=DEFAULT_CLUSTER_NAME, rows=65000000, threads=300, strategy=None):
     config = create_baseline_config(title)
     config['cluster'] = cluster
 
-    schema_options = 'replication=\(factor=3\)'
+    schema_options = 'replication\(factor=3\)'
     if strategy:
-        schema_options += ' compaction\(strategy={strategy}'.format(strategy=strategy)
+        schema_options += ' compaction\(strategy={strategy}\)'.format(strategy=strategy)
 
     config['operations'] = [
         {'operation': 'stress',
-         'command': 'write n={rows} -rate threads={threads} -schema {schema_options}'.format(rows=rows, threads=threads,
-                                                                                             schema_options=schema_options)},
+         'command': 'write n={rows} -rate threads={threads} '
+                    '-schema {schema_options}'.format(rows=rows, threads=threads,
+                                                      schema_options=schema_options)},
         {'operation': 'nodetool',
          'command': 'flush'},
         {'operation': 'nodetool',
@@ -113,36 +131,26 @@ def compaction_strategies_profile(title='Compaction Strategy', cluster='blade_11
     scheduler = Scheduler(CSTAR_SERVER)
     scheduler.schedule(config)
 
+
 def test_STCS_profile():
-    compaction_strategies_profile(title='STCS', strategy='SizeTieredCompactionStrategy')
+    compaction_strategies_profile(title='STCS', strategy='org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy',
+                                  rows='10M')
+
 
 def test_DTCS_profile():
-    compaction_strategies_profile(title='DTCS', strategy='DateTieredCompactionStrategy')
+    compaction_strategies_profile(title='DTCS', strategy='org.apache.cassandra.db.compaction.DateTieredCompactionStrategy',
+                                  rows='10M')
+
 
 def test_LCS_profile():
-    compaction_strategies_profile(title='LCS', strategy='LeveledCompactionStrategy')
+    compaction_strategies_profile(title='LCS', strategy='org.apache.cassandra.db.compaction.LeveledCompactionStrategy',
+                                  rows='10M')
+
 
 def test_commitlog_sync_settings():
     yaml = '\n'.join(['commitlog_sync: batch',
                       'commitlog_sync_batch_window_in_ms: 2',
                       'commitlog_sync_period_in_ms: null',
                       'concurrent_writes: 64'])
-    test_simple_profile(title='Batch Commitlog', yaml=yaml)
-
-def number_of_columns_profile(title='Read/Write {columns}', cluster='blade_11', rows=65000000, threads=300, columns=50):
-    config = create_baseline_config(title.format(columns=columns))
-    config['cluster'] = cluster
-    config['operations'] = [
-        {'operation':'stress',
-         'command': 'write n={rows} -rate threads={threads} -col n=FIXED\({columns}\)'.format(rows=rows, threads=threads, columns=columns)},
-        {'operation':'stress',
-         'command': 'read n={rows} -rate threads={threads}'.format(rows=rows, threads=threads)},
-        {'operation':'stress',
-         'command': 'read n={rows} -rate threads={threads}'.format(rows=rows, threads=threads)}
-    ]
-
-    scheduler = Scheduler(CSTAR_SERVER)
-    scheduler.schedule(config)
-
-def test_fifty_columns_profile():
-    number_of_columns_profile(columns=50)
+    test_simple_profile(title='Batch Commitlog', yaml=yaml,
+                        load_rows='10M', read_rows='10M')
