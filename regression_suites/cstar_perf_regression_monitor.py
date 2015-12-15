1f9197eae812aa14d35f5023cee12a09c98e5498
Alan Boudreault <alan@alanb.ca>
2015-12-14 21:18:56 -0500

    Finalized cstar_perf_regresson_monitor

--- regression_suites/cstar_perf_regression_monitor.py
+++ regression_suites/cstar_perf_regression_monitor.py
@@ -2,15 +2,22 @@
 
 from __future__ import unicode_literals
 
+import time
 import sys
 import argparse
 import requests
 import json
+import logging
+import traceback
 from Queue import Queue
 from threading import Thread
 
-tmpstart = 1448946000
-tmpstop = 1451624399
+logging.basicConfig()
+logger = logging.getLogger('cstar_perf_regression_monitor')
+logger.setLevel(logging.INFO)
+
+DEFAULT_WINDOW_TIME = 90  # days
+DEFAULT_NUMBER_LAST_RUNS = 5  # the last 2 runs
 
 
 class CstarPerfClient(object):
@@ -32,6 +39,64 @@ class CstarPerfClient(object):
         url = self.urls[url].format(**kwargs)
         return "http://{}{}".format(self.server, url)
 
+    def get(self, url):
+        r = requests.get(url)
+        if r.status_code != 200:
+            raise Exception('Error while doing request: {}'.format(url))
+
+        return r.text
+
+
+class CstarTestJob(CstarPerfClient):
+
+    job_id = None
+
+    # metrics list we are interested in
+    # '95th_percentile', '999th_percentile', '99th_percentile','latency_max', 'latency_mean', 'latency_median'
+    metrics_list = ['op rate']
+
+    # job metrics per operation
+    metrics = None
+
+    def __init__(self, job_id, **kwargs):
+        super(CstarTestJob, self).__init__(**kwargs)
+        self.job_id = job_id
+        self.metrics = {}
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __unicode__(self):
+        return self.__str__()
+
+
+    def __str__(self):
+        return "<CstarTestJob({})>".format(self.job_id)
+
+    def __eq__(self, other):
+        return self.job_id == other.job_id
+
+    def _read_job_metrics(self, data):
+        """Read the job metrics"""
+
+        for operation in data['stats']:
+            operation_name = operation.get('test', operation['id'])
+            self.metrics[operation_name] = {}
+            s = self.metrics[operation_name]
+            for m in self.metrics_list:
+                s[m] = float(operation['op rate'].split(' ')[0])
+
+    def get_operations(self):
+        return self.metrics.keys()
+
+    def fetch(self):
+        url = self.build_url('get_test_summary', job_id=self.job_id)
+        logger.debug("Fetching job {} from: {}".format(self.job_id, url))
+        # the summary file is small, so we currently keep it in memory
+        response = self.get(url)
+        data = json.loads(response)
+        self._read_job_metrics(data)
+
 
 class RegressionSeries(CstarPerfClient):
     """Represent a regression serie"""
@@ -39,17 +104,19 @@ class RegressionSeries(CstarPerfClient):
     # Serie name
     name = None
 
-    # wall time
+    # window time
     start_timestamp = None
     stop_timestamp = None
 
     # Job ids of the serie
-    job_ids = None
+    jobs = None
 
-    # Computed metrics
+    # job metrics
     metrics = None
 
-    has_regression = False
+    # Current performance and Historical Performance used to detect regressions
+    current_performance = None
+    historical_performance = None
 
     def __init__(self, name, start_timestamp, stop_timestamp, **kwargs):
         super(RegressionSeries, self).__init__(**kwargs)
@@ -57,106 +124,93 @@ class RegressionSeries(CstarPerfClient):
         self.name = name
         self.start_timestamp = start_timestamp
         self.stop_timestamp = stop_timestamp
-        self.job_ids = []
+        self.jobs = []
 
         self.metrics = {}  # metrics per operation
 
     def __repr__(self):
-        return "<RegressionSeries({}, {} tests)>".format(
-            self.name, len(self.job_ids))
+        return self.__str__()
 
     def __unicode__(self):
         return self.__str__()
 
     def __str__(self):
         return "{}: {} tests)".format(
-            self.name, len(self.job_ids))
+            self.name, len(self.job))
 
-    def _get_series(self):
+    def fetch(self):
         url = self.build_url('get_series', name=self.name,
-            start_timestamp=self.start_timestamp, stop_timestamp=self.stop_timestamp)
-        r = requests.get(url)
-        if r.status_code != 200:
-            raise Exception('Unable to fetch series')
+                             start_timestamp=self.start_timestamp,
+                             stop_timestamp=self.stop_timestamp)
+        logger.debug("Fetching job ids of series '{}' from: {}".format(self.name, url))
+        response = self.get(url)
 
-        return json.loads(r.text)['series']
+        self.jobs = [CstarTestJob(s, server=self.server) for s in json.loads(response)['series']]
+        for job in self.jobs:
+            job.fetch()
 
-    def _fetch_job_stats(self, job_id):
-        url = self.build_url('get_test_summary', name=self.name, job_id=job_id)
-        # the summary file is small, so we currently keep it in memory
-        r = requests.get(url)
-        data = json.loads(r.text)
+    def _compute_performance(self, jobs):
+        """Compute the average performance of a subset of jobs
 
-        stats = {}
-        for operation in data['stats']:
-            operation_name = operation.get('test', operation['id'])
-            stats[operation_name] = {}
-            s = stats[operation_name]
-            s['op_rate'] = float(operation['op rate'].split(' ')[0])
-            s['95th_percentile'] = float(operation['latency 95th percentile'].split(' ')[0])
-            s['999th_percentile'] = float(operation['latency 99.9th percentile'].split(' ')[0])
-            s['99th_percentile'] = float(operation['latency 99th percentile'].split(' ')[0])
-            s['latency_max'] = float(operation['latency max'].split(' ')[0])
-            s['latency_mean'] = float(operation['latency mean'].split(' ')[0])
-            s['latency_median'] = float(operation['latency median'].split(' ')[0])
+        It throws out the fastest and slowest jobs.
+        """
 
-        return stats
+        if not len(jobs):
+            return {}
 
-    def _fetch_series_stats(self):
-        for job_id in self.job_ids:
-            stats = self._fetch_job_stats(job_id)
-            for operation_name, metrics in stats.iteritems():
-                if operation_name not in self.metrics:
-                    self.metrics[operation_name] = {}
-                    self.metrics[operation_name]['op_rate'] = []
-                    self.metrics[operation_name]['95th_percentile'] = []
-                    self.metrics[operation_name]['999th_percentile'] = []
-                    self.metrics[operation_name]['99th_percentile'] = []
-                    self.metrics[operation_name]['latency_max'] = []
-                    self.metrics[operation_name]['latency_mean'] = []
-                    self.metrics[operation_name]['latency_median'] = []
+        average_performance = {}
+        operations = jobs[0].get_operations()
 
-                self.metrics[operation_name]['op_rate'].append(metrics['op_rate'])
-                self.metrics[operation_name]['95th_percentile'].append(metrics['95th_percentile'])
-                self.metrics[operation_name]['999th_percentile'].append(metrics['999th_percentile'])
-                self.metrics[operation_name]['99th_percentile'].append(metrics['99th_percentile'])
-                self.metrics[operation_name]['latency_max'].append(metrics['latency_max'])
-                self.metrics[operation_name]['latency_mean'].append(metrics['latency_mean'])
-                self.metrics[operation_name]['latency_median'].append(metrics['latency_median'])
+        for operation in operations:
+            average_performance[operation] = {}
+            jobs_tmp = list(jobs)
+            # Remove the fastest and slowest job
+            if len(jobs_tmp) > 2:
+                fastest_job = max(jobs_tmp, key=lambda j: j.metrics[operation]['op rate'])
+                slowest_job = min(jobs_tmp, key=lambda j: j.metrics[operation]['op rate'])
+                jobs_tmp.remove(fastest_job)
+                jobs_tmp.remove(slowest_job)
+
+            op_rates = map(lambda j: j.metrics[operation]['op rate'], jobs_tmp)
+            average_op_rate = sum(op_rates) / len(jobs_tmp)
+            average_performance[operation]['op rate'] = average_op_rate
+
+        return average_performance
+
+    def _compute_current_performance(self):
+        """Compute the current performance per operation using the last N runs"""
+
+        jobs = self.jobs[-DEFAULT_NUMBER_LAST_RUNS:]
+        return self._compute_performance(jobs)
 
     def do_regression_check(self):
-        self.job_ids = self._get_series()
+        self.fetch()
 
-        latest_job = self.job_ids.pop()
-        num_jobs = len(self.job_ids)
-
-        self._fetch_series_stats()
-
-        # check all operations for regression
-        stats = self._fetch_job_stats(latest_job)
-
-        for operation_name, metrics in stats.iteritems():
-            op_rate = metrics['op_rate']
-            average_rate = sum(self.metrics[operation_name]['op_rate'])/num_jobs
-
-            # 15% of tolerance... will be configurable
-            if abs(op_rate - average_rate) > (average_rate * 0.15):
-                self.has_regression = True
+        self.current_performance = self._compute_current_performance()
+        self.historical_performance = self._compute_performance(self.jobs)
 
 
 class RegressionMonitor(CstarPerfClient):
     """cstar_perf regression monitor tool"""
 
+    tolerance = 10  # performance deviation tolerance in %
+
     concurrency = 1
+
+    def __init__(self, start_timestamp, stop_timestamp, **kwargs):
+        super(RegressionMonitor, self).__init__(**kwargs)
+
+        self.start_timestamp = start_timestamp
+        self.stop_timestamp = stop_timestamp
 
     def _get_series(self):
         url = self.build_url('get_series_list')
-        r = requests.get(url)
-        if r.status_code != 200:
-            raise Exception('Unable to fetch series list')
+        logger.debug("Fetching series from: {}".format(url))
+        response = self.get(url)
 
-        series = json.loads(r.text)
-        return [RegressionSeries(s, tmpstart, tmpstop, server=self.server) for s in series]
+        series = json.loads(response)
+        return [RegressionSeries(s, self.start_timestamp, self.stop_timestamp,
+                                 server=self.server) for s in series]
 
     def run(self):
         series = self._get_series()
@@ -168,7 +222,13 @@ class RegressionMonitor(CstarPerfClient):
         def worker():
             while True:
                 serie = q.get()
-                serie.do_regression_check()
+
+                try:
+                    serie.do_regression_check()
+                except Exception as e:
+                    logger.error(e)
+                    logger.error(traceback.print_exc())
+
                 q.task_done()
 
         for i in range(self.concurrency):
@@ -179,14 +239,34 @@ class RegressionMonitor(CstarPerfClient):
         q.join()
 
         for serie in series:
-            if serie.has_regression:
-                print 'we have a problem'
+            for operation_name, metrics in serie.current_performance.iteritems():
+                op_rate = metrics['op rate']
+                average_rate = serie.historical_performance[operation_name]['op rate']
+
+                has_regression = False
+                if abs(op_rate - average_rate) > (average_rate * self.tolerance):
+                    has_regression = True
+
+                logger.info(("Regression check for series '{}' operation '{}': "
+                             "historical({}) - current({}) -- {}").format(
+                                 serie.name, operation_name, int(average_rate), int(op_rate),
+                                 'REGRESSION DETECTED' if has_regression else 'OK'))
+
 
 def main(args):
+
+    start_timestamp = args.start_timestamp
+    stop_timestamp = args.stop_timestamp
+
+    if not start_timestamp and not stop_timestamp:
+        time_delta = DEFAULT_WINDOW_TIME * 24 * 60 * 60
+        stop_timestamp = int(time.time())
+        start_timestamp = stop_timestamp - time_delta
+
     monitor = RegressionMonitor(
-        server=args.server,
-        start_timestamp=start_timestamp,
-        stop_timestamp=stop_timestamp
+        start_timestamp,
+        stop_timestamp,
+        server=args.server
     )
 
     if args.command == 'run':
@@ -201,8 +281,8 @@ if __name__ == "__main__":
 
     run = parser_subparsers.add_parser('run', description="Run the regression monitoring process")
     run.add_argument('-s', '--server', required=False, help='The hostname of the server')
-    run.add_argument('--start-timestamp', help='The start timestamp')
-    run.add_argument('--stop-timestamp', help='The stop timestamp')
+    run.add_argument('--start-timestamp', default=None, help='The start timestamp')
+    run.add_argument('--stop-timestamp', default=None, help='The stop timestamp')
 
     try:
         args = parser.parse_args()
