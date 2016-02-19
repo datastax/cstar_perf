ce20647a2d95980d965024988c11441f36bf16d8
Philip Thompson <ptnapoleon@gmail.com>
2016-02-19 13:19:48 -0500

    Update fab_cassandra.py

--- tool/cstar_perf/tool/fab_cassandra.py
+++ tool/cstar_perf/tool/fab_cassandra.py
@@ -30,7 +30,7 @@ GIT_REPOS = [
     ('stef1927',      'git://github.com/stef1927/cassandra.git'),
     ('driftx',        'git://github.com/driftx/cassandra.git'),
     ('jeffjirsa',     'git://github.com/jeffjirsa/cassandra.git'),
-    ('aboudreault',   'git://github.com/aboudreault/cassandra.git')
+    ('aboudreault',   'git://github.com/aboudreault/cassandra.git'),
     ('pauloricardomg','git://github.com/pauloricardomg/cassandra')
 ]
 
