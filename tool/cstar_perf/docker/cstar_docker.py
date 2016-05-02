3de389a3a1af678a9b42f77ff0f6d917376264da
Tomasz Lasica <tomasz.lasica@datastax.com>
2016-05-02 14:46:42 -0400

    Remove sudo

--- tool/cstar_perf/docker/cstar_docker.py
+++ tool/cstar_perf/docker/cstar_docker.py
@@ -59,7 +59,7 @@ RUN \
       libjpeg-dev \
       lxc
 
-RUN echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections && \
+RUN echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections && \
     add-apt-repository ppa:webupd8team/java && \
     apt-get update && \
     apt-get install -y \
