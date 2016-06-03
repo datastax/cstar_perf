6c7103dd0259b6e463148c8c87311816862707a2
Jim Witschey <jim.witschey@gmail.com>
2016-06-03 13:53:01 -0400

    remove josh-mckenzie from fab_cassandra.py
    correcting accidental ninja commit

--- tool/cstar_perf/tool/fab_cassandra.py
+++ tool/cstar_perf/tool/fab_cassandra.py
@@ -16,7 +16,6 @@ GIT_REPOS = [
     ('shawnkumar',    'git://github.com/shawnkumar/cassandra.git'),
     ('mambocab',      'git://github.com/mambocab/cassandra.git'),
     ('jbellis',       'git://github.com/jbellis/cassandra.git'),
-    ('josh-mckenzie', 'git@github.com:josh-mckenzie/cassandra.git'),
     ('marcuse',       'git://github.com/krummas/cassandra.git'),
     ('pcmanus',       'git://github.com/pcmanus/cassandra.git'),
     ('belliottsmith', 'git://github.com/belliottsmith/cassandra.git'),
