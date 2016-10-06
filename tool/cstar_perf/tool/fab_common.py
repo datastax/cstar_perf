64790ba50d1ed67f80a2de132bff0144711ab1d5
Chris Splinter <chris.splinter@datastax.com>
2016-10-06 15:15:58 -0700

    add use_sudo to fab.put

--- tool/cstar_perf/tool/fab_common.py
+++ tool/cstar_perf/tool/fab_common.py
@@ -851,7 +851,7 @@ def bash(script, sudo=False):
     fabric_operation = fab.sudo if sudo else fab.run
     fabric_operation('mkdir -p ~/fab/scripts')
     script_path = '~/fab/scripts/{script_name}.sh'.format(script_name=uuid.uuid1())
-    fab.put(script, script_path)
+    fab.put(script, script_path, use_sudo=sudo)
     output = StringIO()
     fabric_operation('bash {script_path}'.format(script_path=script_path), stdout=output, stderr=output)
     output.seek(0)
